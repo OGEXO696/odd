accfab8ddf1e6c94ad13c79e2c4f506b4509500f
OG <120782023+OGEXO696@users.noreply.github.com>
2023-01-12 16:53:14 +0500

    Update main2.js

--- main2.js
+++ main2.js
@@ -4,6 +4,9 @@
 //     if( i % 2 != 0 ) {
 //     console.log(i);}
 // }
+
+
+
 // for (let i = 0; i < numArr.length; i++) {
 //     // answer += numArr[i] % 2 == 0 ? 1 : 0;
 //     // answer += numArr[i] % 2 != 0 ? 1 : 0;
@@ -13,10 +16,14 @@
 
 // console.log(answer);
 
+
+
+
 const numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
 const isOdd =(num) => num % 2 == 0;
 const odd = numbers.filter(isOdd);
 console.log(odd );
+
 
 // function check(num) {
 //     return(-1)**num+1?'juft son':'toq son';
